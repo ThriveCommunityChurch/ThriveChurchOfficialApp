e8a5a037b8d948b5b4bfd2c0a6b7cd60e41d5f1f
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-08-06 21:43:04 -0400

    Staying committed .... huh get it?

---
+++ Thrive Community Church/MainViewControllers/More/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Thrive Church Official App
+//
+//  Created by Wyatt Baggett on 8/6/17.
+//  Copyright © 2017 Thrive Community Church. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
