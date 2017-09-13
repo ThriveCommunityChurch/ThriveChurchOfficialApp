447e0b2143e593ccd5bfd4ecd0af33369ce6e2dc
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-12 23:42:42 -0400

    1.5.1 - WIP

--- Thrive Community Church/AppDelegate.swift
+++ Thrive Community Church/AppDelegate.swift
@@ -26,26 +26,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
         FirebaseApp.configure()
         
         // Registering notifications
-//        if #available(iOS 10.0, *) {
-//            // For iOS 10 display notification (sent via APNS)
-//            UNUserNotificationCenter.current().delegate = self
-//
-//            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//            UNUserNotificationCenter.current().requestAuthorization(
-//                options: authOptions,
-//                completionHandler: {_, _ in })
-//        } else {
-//            let settings: UIUserNotificationSettings =
-//                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-//            application.registerUserNotificationSettings(settings)
-//        }
-//
-//        application.registerForRemoteNotifications()
-//
-//        let token = Messaging.messaging().fcmToken
-//        print("FCM token: \(token ?? "")")
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+
+        let token = Messaging.messaging().fcmToken
+        print("FCM token: \(token ?? "")")
         
-        // End registration
+     //End registration
         
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
@@ -119,10 +119,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
     
 //*****************************************PushNotifications***********************************************************
     
-//    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
-//        print("Firebase registration token: \(fcmToken)")
-//    }
-//
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+    }
+
 //    func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
 //        print(remoteMessage.appData)
 //    }
