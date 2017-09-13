2f47703ad660a70f8362266284a18f928b721603
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-12 23:21:46 -0400

    Removing & Updating Pods

--- Podfile
+++ Podfile
@@ -8,8 +8,6 @@ target 'Thrive Church Official App' do
   # Pods for Thrive Church Official App
   pod 'Firebase/Core'
   pod 'Firebase/Messaging'
-  pod 'Firebase/Crash'
-  pod 'Firebase/DynamicLinks'
 
   target 'Thrive Church Official AppTests' do
     inherit! :search_paths
--- Thrive Community Church/AppDelegate.swift
+++ Thrive Community Church/AppDelegate.swift
@@ -26,24 +26,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
         FirebaseApp.configure()
         
         // Registering notifications
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-
-        let token = Messaging.messaging().fcmToken
-        print("FCM token: \(token ?? "")")
+//        if #available(iOS 10.0, *) {
+//            // For iOS 10 display notification (sent via APNS)
+//            UNUserNotificationCenter.current().delegate = self
+//
+//            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+//            UNUserNotificationCenter.current().requestAuthorization(
+//                options: authOptions,
+//                completionHandler: {_, _ in })
+//        } else {
+//            let settings: UIUserNotificationSettings =
+//                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+//            application.registerUserNotificationSettings(settings)
+//        }
+//
+//        application.registerForRemoteNotifications()
+//
+//        let token = Messaging.messaging().fcmToken
+//        print("FCM token: \(token ?? "")")
         
         // End registration
         
@@ -119,9 +119,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
     
 //*****************************************PushNotifications***********************************************************
     
-    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
-        print("Firebase registration token: \(fcmToken)")
-    }
+//    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+//        print("Firebase registration token: \(fcmToken)")
+//    }
+//
+//    func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+//        print(remoteMessage.appData)
+//    }
     
     func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
         print("The message is: \(remoteMessage.appData)")
