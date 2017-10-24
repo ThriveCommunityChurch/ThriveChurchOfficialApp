d0621cc85aab6447fcb3d78a4985416565d039e5
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-24 19:31:08 -0400

    Works but destroys your chance of getting notifs
    Removed setting that would break the notif from ever reaching your device... I think mine isn't gonna ever get them now for some reason.

--- Thrive Community Church/AppDelegate.swift
+++ Thrive Community Church/AppDelegate.swift
@@ -146,7 +146,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
     func userNotificationCenter(_ center: UNUserNotificationCenter,  willPresent notification: UNNotification,
                 withCompletionHandler completionHandler: @escaping (_ options: UNNotificationPresentationOptions) -> Void) {
 
-        // custom code to handle push while app is in the foreground
+        // Handle push while app is in the foreground
         print("Handle push from foreground\(notification.request.content.userInfo)")
 
         let dict = notification.request.content.userInfo["aps"] as! NSDictionary
@@ -154,17 +154,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, UN
         let body : String = d["body"] as! String
         let title : String = d["title"] as! String
         print("Title:\(title) + body:\(body)")
-        self.showLocalAlert(title: title, message:body, buttonTitle:"Dismiss", window:self.window!)
-
+        self.showLocalAlert(title: title, message: body, buttonTitle: "Dismiss", window: self.window!)
     }
     
     @available(iOS 10.0, *)
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) {
-        // if you set a member variable in didReceiveRemoteNotification, you  will know if this is from closed or background
+        // If you set a member variable in didReceiveRemoteNotification, you will know if this is from closed or background
         print("Handle push from background or closed\(response.notification.request.content.userInfo)")
     }
     
-    //show local notif if the application is in the Foreground
+    // Show local notif if the application is in the Foreground
     func showLocalAlert(title: String, message: String, buttonTitle: String, window: UIWindow) {
         print("Foreground?")
         let alert = UIAlertController(title: title,
