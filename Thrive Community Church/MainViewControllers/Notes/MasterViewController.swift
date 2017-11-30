0402c799d42341f6e19fffe4c900ce1d797ee7f2
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-29 22:26:26 -0500

    Removing the note note

--- Thrive Community Church/MainViewControllers/Notes/MasterViewController.swift
+++ Thrive Community Church/MainViewControllers/Notes/MasterViewController.swift
@@ -134,10 +134,12 @@ class MasterViewController: UITableViewController {
         return true
     }
     
+    // TODO: Find where the deletion is made
     override func tableView(_ tableView: UITableView, commit
                                         editingStyle: UITableViewCellEditingStyle,
                                         forRowAt indexPath: IndexPath) {
         
+        // Possibly here is where the deletion is made - kinda obvious
         if editingStyle == .delete {
             objects.remove(at: (indexPath as NSIndexPath).row)
             tableView.deleteRows(at: [indexPath], with: .fade)
