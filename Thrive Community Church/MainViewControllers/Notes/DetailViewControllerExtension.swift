095290ed2501f449e169b8cbdd0ab3efb600b7f8
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-29 22:31:47 -0500

    Removing label as specified in issue #55

--- Thrive Community Church/MainViewControllers/Notes/DetailViewControllerExtension.swift
+++ Thrive Community Church/MainViewControllers/Notes/DetailViewControllerExtension.swift
@@ -12,17 +12,17 @@ import Firebase
 extension DetailViewController {
     
     // Might change this to be something using Auth instead - but in the meantime this works
-    func uploadToFirebase(label: String) {
+    func uploadToFirebase() {
             
             // send this to the DB
             self.ref = Database.database().reference().child("notes")
             let key = self.ref.childByAutoId().key
                                             
             let note = ["id":key,
-                        "label": label,
                         "note": detailDescriptionLabel.text!
             ]
-            //adding the artist inside the generated unique key
+        
+            //adding the note inside the generated key
             self.ref.child(key).setValue(note)
     }
 }
