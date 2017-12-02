252bd16f0592225a05a2a37c857d117cb5acdf0c
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-12-02 14:47:54 -0500

    Fixing Codacy issues

--- Thrive Community Church/MainViewControllers/Bible/ReadActsViewController.swift
+++ Thrive Community Church/MainViewControllers/Bible/ReadActsViewController.swift
@@ -20,7 +20,7 @@ class ReadActsViewController: UIViewController, UIWebViewDelegate {
         loadActsView()
     }
     
-    private func loadActsView(){
+    private func loadActsView() {
         let url = URL(string: "https://www.bible.com/bible/59/act.1")
         let request = URLRequest(url: url!)
         
@@ -31,14 +31,15 @@ class ReadActsViewController: UIViewController, UIWebViewDelegate {
         super.didReceiveMemoryWarning()
     }
     
-    func webViewDidStartLoad(_ Webview: UIWebView) {
+    func webViewDidStartLoad(_ webView: UIWebView) {
         loading.startAnimating()
         print("Loading....")
         
     }
     
-    func webViewDidFinishLoad(_ Webview: UIWebView) {
+    func webViewDidFinishLoad(_ webView: UIWebView) {
         loading.stopAnimating()
         print("Stopped Loading!")
     }
+    
 }
