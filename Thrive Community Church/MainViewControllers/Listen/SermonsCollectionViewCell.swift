00275e515c91c76a40089ad9aa857fd30ab22f7a
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-12-19 22:25:20 -0500

    WIP on programmatic series, wanna use a collection VC

---
+++ Thrive Community Church/MainViewControllers/Listen/SermonsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SermonsCollectionViewCell.swift
+//  Thrive Church Official App
+//
+//  Created by Wyatt Baggett on 12/19/18.
+//  Copyright © 2018 Thrive Community Church. All rights reserved.
+//
+
+import UIKit
+
+class SermonsCollectionViewCell: UICollectionViewCell {
+    
+	@IBOutlet weak var seriesArt: UIImageView!
+	
+	
+}
