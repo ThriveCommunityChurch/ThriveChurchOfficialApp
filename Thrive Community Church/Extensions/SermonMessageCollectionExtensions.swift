c7451cb74c21f8e9b7c9c318e399541303fbc083
Wyatt Baggett <wyattbaggett@yahoo.com>
2020-04-25 03:17:24 -0400

    This seems like a bug with the OS, this exception doesn't make sense
    I am not modifying the contents of the UI from a background thread. It seems very intermittent and not sure if I want to live with that feature because of that

---
+++ Thrive Community Church/Extensions/SermonMessageCollectionExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  SermonMessageCollectionExtensions.swift
+//  Thrive Church Official App
+//
+//  Created by Wyatt Baggett on 4/25/20.
+//  Copyright © 2020 Thrive Community Church. All rights reserved.
+//
+
+import Foundation
